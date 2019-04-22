c5f869521c3c585c7d7da7476bad78e2b0aae6a3
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-22 17:23:43 -0600

    Updated v4

--- Part1/index1.js
+++ Part1/index1.js
@@ -4,6 +4,8 @@ function play(clickedId) {
     const playerSpan = document.getElementById('player');
     const clickedElement = document.getElementById(clickedId);
 
+    let winnerMessage = `PLAYER ${playerSpan.innerText} WINS!`
+
     if (playerSpan.innerText === 'X') {
         playerSpan.innerText = 'O';
         clickedElement.innerText = 'X';
@@ -13,55 +15,17 @@ function play(clickedId) {
         clickedElement.innerText = 'O';
         board[clickedId] = 'O';
     }
-    console.log(board)
+
+    // [0,1,2]
+    // [3,4,5]
+    // [6,7,8]
+
+    if (board[0] !== undefined && board[0] === board[1] && board[0] === board[2]) {alert(winnerMessage)} // ROW ONE
+    if (board[3] !== undefined && board[3] === board[4] && board[3] === board[5]) {alert(winnerMessage)} // ROW TWO
+    if (board[6] !== undefined && board[6] === board[7] && board[6] === board[8]) {alert(winnerMessage)} // ROW THREE
+    if (board[0] !== undefined && board[0] === board[3] && board[0] === board[6]) {alert(winnerMessage)} // COL ONE
+    if (board[1] !== undefined && board[1] === board[4] && board[1] === board[7]) {alert(winnerMessage)} // COL TWO
+    if (board[2] !== undefined && board[2] === board[5] && board[2] === board[8]) {alert(winnerMessage)} // COL THREE
+    if (board[0] !== undefined && board[0] === board[4] && board[0] === board[8]) {alert(winnerMessage)} // DIA ONE
+    if (board[6] !== undefined && board[6] === board[4] && board[6] === board[2]) {alert(winnerMessage)} // DIA TWO
 }
-
-// [1,2,3]
-// [4,5,6]
-// [7,8,9]
-
-if (t1 !== undefined && t1 === t2 && t1 === t3) {
-    alert(`${t1} winner`);
-}
-
-if (m4 !== undefined && m4 === m5 && m4 === m6) {
-    alert(`$(m4) winner`);
-}
-
-if (b7 !== undefined && b7 === b5 && b4 === b6) {
-    alert(`$(b7) winner`);
-}
-
-if (t2 !== undefined && t2 === t1 && t2 === t3) {
-    alert(`$(t2) winner`);
-}
-
-if (m5 !== undefined && m5 === m4 && m5 === m6) {
-    alert(`$(m5) winner`);
-}
-
-if (b8 !== undefined && b8 === b7 && b8 === b9) {
-    alert(`$(b8) winner`);
-}
-
-if (t3 !== undefined && t3 === t2 && t3 === t1) {
-    alert(`$(t3) winner`);
-}
-
-if (m6 !== undefined && m6 === m5 && m6 === m4) {
-    alert(`$(m6) winner`);
-}
-
-if (b9 !== undefined && b9 === b8 && b9 === b7) {
-    alert(`$(b9) winner`);
-}
-
-const t1 = board[0];
-const t2 = board[1];
-const t3 = board[2];
-const m4 = board[3];
-const m5 = board[4];
-const m6 = board[5];
-const b7 = board[6];
-const b8 = board[7];
-const b9 = board[8];
