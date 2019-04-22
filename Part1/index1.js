4cb42b29f6270e59beffcf47048949369bbc9839
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-22 16:51:27 -0600

    Update v3

--- Part1/index1.js
+++ Part1/index1.js
@@ -16,22 +16,52 @@ function play(clickedId) {
     console.log(board)
 }
 
+// [1,2,3]
+// [4,5,6]
+// [7,8,9]
 
-if (topRight !== undefined && topRight === topCenter && topRight === topRight) {
-    alert(`$(topRight) winner`); 
+if (t1 !== undefined && t1 === t2 && t1 === t3) {
+    alert(`${t1} winner`);
 }
 
-if (topCenter !== undefined && topCenter === topRight && topRight === topCenter) {
-    alert(`$(topCenter) winner`);
+if (m4 !== undefined && m4 === m5 && m4 === m6) {
+    alert(`$(m4) winner`);
 }
 
+if (b7 !== undefined && b7 === b5 && b4 === b6) {
+    alert(`$(b7) winner`);
+}
 
-const topLeft = board[0];
-const topCenter = board[1];
-const topRight = board[2];
-const middleLeft = board[3];
-const middleCenter = board[4];
-const middleRight = board[5];
-const bottomLeft = board[6];
-const bottomCenter = board[7];
-const bottomRight = board[8];
+if (t2 !== undefined && t2 === t1 && t2 === t3) {
+    alert(`$(t2) winner`);
+}
+
+if (m5 !== undefined && m5 === m4 && m5 === m6) {
+    alert(`$(m5) winner`);
+}
+
+if (b8 !== undefined && b8 === b7 && b8 === b9) {
+    alert(`$(b8) winner`);
+}
+
+if (t3 !== undefined && t3 === t2 && t3 === t1) {
+    alert(`$(t3) winner`);
+}
+
+if (m6 !== undefined && m6 === m5 && m6 === m4) {
+    alert(`$(m6) winner`);
+}
+
+if (b9 !== undefined && b9 === b8 && b9 === b7) {
+    alert(`$(b9) winner`);
+}
+
+const t1 = board[0];
+const t2 = board[1];
+const t3 = board[2];
+const m4 = board[3];
+const m5 = board[4];
+const m6 = board[5];
+const b7 = board[6];
+const b8 = board[7];
+const b9 = board[8];
